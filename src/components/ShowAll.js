033fc681b2bd87bbc58fe2304efb0ca1b3e96a18
Marlon Gumban <msg2msg2@gmail.com>
2017-08-20 11:59:00 +0800

    .

--- src/components/ShowAll.js
+++ src/components/ShowAll.js
@@ -4,12 +4,21 @@ var App = require("./App");
 var Choices = require("./Choices");
 
 class ShowAll extends React.Component {
+    constructor(props) {
+        super(props);
+        this.HomepageBtnClick = this.HomepageBtnClick.bind(this);
+    }
+
+    HomepageBtnClick() {
+
+    }
+
     render() {
         return (
             <div>
             <Banner /> <br />
             <Choices /> <br />
-            <App />
+            <App onHomepageBtnClick = {this.HomepageBtnClick}/>
             </div>
         );
     }
