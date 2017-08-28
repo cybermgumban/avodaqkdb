bff0cdc63f9f2453cb4ea735934190e12e870de3
Marlon Gumban <msg2msg2@gmail.com>
2017-08-29 07:43:39 +0800

    doing the searchresult

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,25 @@
 var React = require("react");
 
+var dbjabber = require("../db/dbjabber");
+
+var searchStyle = {
+    borderStyle: "dashed",
+    borderColor: "#bfbfbf",
+    marginRight: "-10px",
+    marginLeft: "-10px",
+    height: "5px",
+    width: "auto",
+    textAlign: "right",
+};
+
 class Search extends React.Component {
     constructor(props){
         super(props);
 
         this.state = {
             type: "Jabber",
-            keyword: ""
+            keyword: "",
+            searchResult: []
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -22,8 +35,19 @@ class Search extends React.Component {
     }
 
     handleSubmit(event) {
-        alert(event.target.value);
-    }
+            if(this.state.type==="Jabber") {
+                alert(event.target.value);
+                var searchResult = [];
+                searchResult = (
+                    <h2>Search Result:</h2>,
+                    dbjabber.map((jabber, index) => (
+                        <p key={index}>{dbjabber.title}</p>
+                    )))
+                this.setState ({
+                    searchResult: searchResult
+                })
+            }
+        }
 
     render() {
             console.log(this.state.type);
@@ -53,6 +77,10 @@ class Search extends React.Component {
                     <input type="submit" value="Search" onClick={this.handleSubmit}/>
                     </label>
                 </form>
+                    <br/>
+                <div style={searchStyle}>
+                </div>
+                    {this.state.searchResult}
             </div>
         );
     }
