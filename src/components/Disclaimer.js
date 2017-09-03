31ed417d4861d9398db48b61bd753724202c3877
Marlon Gumban <msg2msg2@gmail.com>
2017-09-03 12:07:34 +0800

    .

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -8,7 +8,12 @@ class Disclaimer extends React.Component {
             <h1 style={{paddingTop: "100px"}}>
                 This avodaq Knowledge Database purpose is to have a central database for all ASC guides that will greatly help new and existing ASC engineers.                
             </h1>
-            <p style={{marginTop:"120px"}}>Developed by: Marlon Gumban</p>
+            <br /><br/> <br />
+            <h3 style={{textAlign:"left"}}>Future updates / improvement:</h3>
+            <li style={{textAlign:"left"}}>Addition of ticket tagging (e.g. OTRS, TAC, Webex tickets)</li>
+            <li style={{textAlign:"left"}}>Able to show images and numbered lists</li>
+            <li style={{textAlign:"left"}}>Able to Add, Edit and Delete into the database itself</li>
+            <p style={{marginTop:"120px", fontSize: "20px"}}>Coded by: Marlon Gumban</p>
             </div>
         );
     }
